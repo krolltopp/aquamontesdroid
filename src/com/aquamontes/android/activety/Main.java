6f488c2faafd3494b9bfdc517c6df9a92c9db9a6
anders <anders.froyhaug@i-nor.com>
2014-05-15 21:15:43 +0200

    clean slate

---
+++ src/com/aquamontes/android/activety/Main.java
@@ -0,0 +1,18 @@
+package com.aquamontes.android.activety;
+
+import com.aquamontes.R;
+import com.aquamontes.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
